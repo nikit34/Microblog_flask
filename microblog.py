7e139457619490e9e6302d8d6160c3bd1e76f592
nikit34 <permikov134@gmail.com>
2019-10-01 07:49:45 +0300

    second

--- microblog.py
+++ microblog.py
@@ -1,1 +1,7 @@
-from src import app
+from src import app, db
+from src.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
